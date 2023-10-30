7d27e8906dfb152ac51efa2ffa214ef4bfc5a58f
Ashures <ashuresyt@gmail.com>
2023-10-30 20:17:00 +1300

    Added urlencoding and json formatting to app

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { urlencoded, json } from "express";
 import indexRoutes from "./routes/index.js";
+import apiRoutes from "./routes/api/index.js";
 
 const app = express();
 
+app.use(urlencoded({ "extended": false }));
+app.use(json());
+
 app.use("/", indexRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
