1ea152de055ea3bc3d1a70cbf3f16d68fa1a4dbc
Ashures <ashuresyt@gmail.com>
2023-10-30 19:56:25 +1300

    Added API call handlers to user controller

---
+++ controllers/user.js
@@ -0,0 +1,87 @@
+import { PrismaClient } from "@prisma/client";
+import apiShell from "../utils/apiShell";
+
+const prisma = new PrismaClient();
+
+const createUser = async (req, res) => {
+  await apiShell(res, async () => {
+    await prisma.user.create({
+      "data": { ...req.body },
+    });
+
+    const newUser = await prisma.user.findUnique({
+      "where": { "id": Number(req.params.id) },
+    });
+
+    return res.status(200).json({
+      "msg": "User successfully created!",
+      "data": newUser,
+    });
+  });
+};
+
+const getUsers = async (req, res) => {
+  await apiShell(res, async () => {
+    const users = await prisma.user.findMany();
+
+    if (users.length === 0) return res.status(404).json({ "msg": "No users found" });
+
+    return res.status(200).json({ "data": users, });
+  });
+};
+
+const getUser = async (req, res) => {
+  await apiShell(res, async () => {
+    const user = await prisma.user.findUnique({
+      "where": { "id": Number(req.params.id) }
+    });
+
+    if (!user) return res.status(404).json({ "msg": `No user with id: ${req.params.id} found` });
+
+    return res.status(200).json({ "data": user, });
+  });
+};
+
+const updateUser = async (req, res) => {
+  await apiShell(res, async () => {
+    let user = await prisma.user.findUnique({
+      "where": { "id": Number(req.params.id) }
+    });
+
+    if (!user) return res.status(404).json({ "msg": `No user with id: ${req.params.id} found` });
+
+    user = await prisma.user.update({
+      "where": { "id": Number(req.params.id) },
+      "data": { ...req.body },
+    });
+
+    return res.status(200).json({ 
+      "msg": `User with id: ${req.params.id} was updated successfully!`,
+      "data": user,
+    });
+  });
+};
+
+const deleteUser = async (req, res) => {
+  await apiShell(res, async () => {
+    const user = await prisma.user.findUnique({
+      "where": { "id": Number(req.params.id) }
+    });
+
+    if (!user) return res.status(404).json({ "msg": `No user with id: ${req.params.id} found` });
+
+    await prisma.user.delete({
+      "where": { "id": Number(req.params.id) },
+    });
+
+    return res.status(200).json({ "msg": `User with id: ${req.params.id} was deleted successfully!`, });
+  });
+};
+
+export {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
+};
