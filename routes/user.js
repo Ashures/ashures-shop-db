7c1cd0ee877ef826b01fdfa1d89c20315f3b1e4a
Ashures <ashuresyt@gmail.com>
2023-10-30 19:58:40 +1300

    Created new routes for user using controller

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
+} from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
