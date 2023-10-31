3cbd39d9a55a2da113a71a650ccb2a1cfe4bd16b
Ashures <ashuresyt@gmail.com>
2023-10-31 13:23:52 +1300

    Added auto-generated default icon on createUser

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,9 @@ import hash from "../utils/hash.js";
 const prisma = new PrismaClient();
 
 const createUser = async (req, res) => {
+  const colours = [ "e7e6f7", "e3d0d8", "aea3b0", "827081", "c6d2ed" ];
+
+  if (req.body["icon"].length !== 0) req.body["icon"]= `https://source.boringavatars.com/ring/120/?colors=${colours[0]},${colours[1]},${colours[2]},${colours[3]},${colours[4]}`;
   req.body["password"] = await hash(req.body["password"]);
 
   await apiShell(res, async () => {
