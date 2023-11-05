d926bec497a5fa02aca8f6d1b6d984fa60a853dc
Ashures <ashuresyt@gmail.com>
2023-11-05 20:44:02 +1300

    Deleted all login files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express, { urlencoded, json } from "express";
 import indexRoutes from "./routes/index.js";
 import apiRoutes from "./routes/api/index.js";
-import loginRoute from "./routes/login.js";
 
 const app = express();
 
@@ -10,7 +9,6 @@ app.use(json());
 
 app.use("/", indexRoutes);
 app.use("/api", apiRoutes);
-app.use("/login", loginRoute);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
